dc091ec224743e3eb181b7ccd8e392364c09e87e
Jose Vallejo <joseavallejo12@gmail.com>
2021-09-12 19:22:11 -0500

    ff

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -1,39 +1,45 @@
 #!/usr/bin/python3
-"""Get request to reddit API"""
+"""
+recursive function that queries the Reddit API
+"""
+
 import requests
-import re
 
 
-def count_words(subreddit, word_list, first_call=True, after="", dic={}):
-    """Cound # of keywords"""
-    if first_call:
-        word_list = list(set(word_list))
-        d = count_words(subreddit, word_list, False)
-        if d:
-            for key, value in sorted(d.items(), key=lambda x: (-x[1], x[0])):
-                print("{}: {}".format(key, value))
-        elif d is None:
-            return None
-    else:
-        base_url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-        h = {'User-Agent': 'Reddit API test'}
-        params = {'limit': 200, 'after': after}
-        r = requests.get(base_url, headers=h,
-                         allow_redirects=False, params=params)
-        if r.status_code != 200:
-            return None
-        d = r.json()
-        if after is None:
-            return dic
-        children = d.get('data', {}).get('children')
-        for child in children:
-            title = child.get('data', {}).get('title').lower()
-            for word in word_list:
-                regex_exp = re.compile(word + '[\s,.]')
-                if re.search(regex_exp, title):
-                    if word not in dic:
-                        dic[word] = 1
-                    else:
-                        dic[word] += 1
-        p = d.get('data', {}).get('after')
-        return count_words(subreddit, word_list, False, p, dic)
+def get_articles(sub, w, wd, wd_r, a="",):
+    """ Get articles """
+    url = "https://www.reddit.com/r/{}".format(sub)
+    url = "{}/hot.json?limit=100&after={}".format(url, a)
+    response = requests.get(url, allow_redirects=False,
+                            headers={'User-agent': 'Holberton'}
+                            )
+    if response.status_code != 200:
+        return None
+    articles = response.json()["data"]["children"]
+    for article in articles:
+        titles = article["data"]["title"].lower().split(" ")
+        for title in titles:
+            if title in wd:
+                wd[title] += 1
+    if response.json()["data"]["after"] is None:
+        sw = sorted(wd.items(), key=lambda t: t[::-1])
+        sw_desc = sorted(sw, key=lambda tup: tup[1], reverse=True)
+        for w in sw_desc:
+            if w[1] > 0:
+                print("{}: {}".format(w[0], w[1] * wd_r[w[0]]))
+        return
+    return get_articles(sub, w, wd, wd_r, response.json()["data"]["after"])
+
+
+def count_words(subreddit, word_list):
+    """ Count words """
+    w_d = {}
+    w_d_r = {}
+    word_list = [word.lower() for word in word_list]
+    for w in word_list:
+        if w not in w_d:
+            w_d_r[w] = 1
+            w_d[w] = 0
+        else:
+            w_d_r[w] += 1
+    get_articles(subreddit, word_list, w_d, w_d_r)
